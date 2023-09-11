44ea3d3dbc0f914ba9f20986b721543d6ef73139
Josepogers <zekreds12@gmail.com>
2023-09-11 16:21:45 -0300

    Aprendo a manipular os templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'recipes/home.html')
+    return render(request,'recipes/home.html', context={ 'name': 'José Francisco',})
 
 def contato(request):
     return HttpResponse('Contato')
